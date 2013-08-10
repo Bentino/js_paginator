b25aeca469ddb33472c5a7b477b6f417c6813af7
bentino <bentino@gmail.com>
2013-08-10 17:40:43 +0700

    responsive layout

--- sample/scripts/paginator.js
+++ sample/scripts/paginator.js
@@ -1,46 +1,35 @@
 ( function($) {
+	
 	$.fn.pagination = function( options ) {
 		
-		var settings = $.extend({
-			item: 1,
-			width: '200px',
-			height: '200px',
-			row: 3,
-			column: 3,
-			margin: '5px',
-			direction: 'vertical',
-			selector: 'li',
-			touch: false,
-			find: false
-		}, options );
-		
-		// Page html construction
-		
+		var settings = $.extend({}, $.fn.pagination.defaults, options );
+
 		var pagination = 1;
 		
-		this.each(function(){
+		var items = null;
+		var pages = null;
 		
-			var items = null;
-			var pages = null;
-			
-			var width = parseInt(settings.width);
-			var height = parseInt(settings.height);
-			var margin = parseInt(settings.margin);
-			var row = parseInt(settings.row);
-			var column = parseInt(settings.column);
-			
-			var page_width = (width + (margin * 2)) * column;
-			var page_height = (height + (margin * 2)) * row;
+		var width = parseInt(settings.width);
+		var height = parseInt(settings.height);
+		var margin = parseInt(settings.margin);
+		var row = parseInt(settings.row);
+		var column = parseInt(settings.column);
+		
+		var page_width = (width + (margin * 2)) * column;
+		var page_height = (height + (margin * 2)) * row;
+		
+		// Page html construction
+		this.each(function(){ 
 
 			$(this).css('display', 'block');
-			$(this).css('height', page_height);
+			$(this).css('min-height', page_height);
 			
 			if(settings.touch) {
 				$(this).css('overflow-x', 'scroll');
-				$(this).css('width', page_width + 30);
+				$(this).css('max-width', page_width + 30);
 			} else {
 				$(this).css('overflow', 'hidden');
-				$(this).css('width', page_width);
+				$(this).css('max-width', page_width);
 			}
 			
 			if(settings.find) {
@@ -92,27 +81,49 @@
 			
 			// Creating page bullet
 			if(!settings.touch) {
-				var html = '<div class="pagination"><ul>';
-				for (var i=0; i < pages; i++) {
-					html += '<li><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+				if(settings.controller) {
+					var html = '<div class="pagination"><ul><li class="prev controller"><button>prev</button></li>';
+					for (var i=0; i < pages; i++) {
+						if(i == 0) {
+							html += '<li class="pager first"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						} else if(i == pages-1) {
+							html += '<li class="pager last"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						} else {
+							html += '<li class="pager"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						}
+					}
+					html += '<li class="next controller"><button>next</button></li></ul></div>';
+				}
+				else {
+					var html = '<div class="pagination"><ul>';
+					for (var i=0; i < pages; i++) {
+						if(i == 0) {
+							html += '<li class="pager first"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						} else if(i == pages-1) {
+							html += '<li class="pager last"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						} else {
+							html += '<li class="pager"><button type="button" value="#pagination-'+ pagination +'_no-'+(i+1)+'">'+(i+1)+'</button></li>';
+						}
+					}
+					html += '</ul></div>';	
 				}
 				
-				html += '</ul></div>';
 			}
 			
 			$(this).after(html);
 			pagination++;
 			
 			// Assigned position
+			var root = $(this).parent();
 			var group = $(this).find('.pagination-group').css('position', 'relative');
 			var items = $(this).find('.views-row');
 			
 			group.css('position', 'relative');
-			group.css('display', 'block')
-			group.css('width', page_width);
+			group.css('display', 'block');
+			group.css('width', $('.viewport-container').parent().width());
 			
-			items.css('width', settings.width);
-			items.css('height', settings.height);
+			items.css('width', '100%');
+			items.css('height', 'auto');
 			items.css('margin', settings.margin);
 			items.css('float', 'left');
 			items.css('overflow', 'hidden');
@@ -120,71 +131,177 @@
 			// Set viewport width for horizontal scrolling
 			if(settings.direction == 'horizontal') {
 				group.css('float', 'left');
-				$(this).find('.viewport-container').css('width', page_width * pages);
+				$(this).find('.viewport-container').css('width', root.width() * pages);
 			} else {
 				group.css('width', page_width);
+				$(this).find('.viewport-container').css('height', root.width() * pages);
 			}
 			
 		}); // End page html construction
 		
 		// Default active state
-		$('.pagination').each(function(){
-			$(this).find('ul li:first').addClass('current');
-		});
-		$('viewport-container').each(function(){
-			$(this).find('.pagination-group:first').addClass('current');
+		var pagination_element = $(this).parent().find('.pagination');
+		var viewport_container_element = $(this).find('.viewport-container');
+		
+		pagination_element.find('ul li.pager:first').addClass('current');
+		viewport_container_element.find('.pagination-group:first').addClass('current');
+		
+		// Event handling
+		pagination_element.on('click', 'li.pager button', function() {
+			switch (settings.direction) {
+				case 'horizontal' :
+					var id = $(this).attr('value');
+			  		var selected = $(id);
+					scrollAnimation( this, settings.direction, selected.width() );
+					break;
+
+				case 'vertical' :
+					var id = $(this).attr('value');
+			  		var selected = $(id);
+					scrollAnimation( this, settings.direction, selected.height() );
+					break;
+			}
 		});
 		
-		// Event handler
-		var paginations = $('.pagination');
+		pagination_element.on('click', 'li.prev button', function() {
+			switch (settings.direction) {
+				case 'horizontal' :
+					var current = $(this).parent().parent().find('.current');
+					var id = current.find('button').attr('value');
+					var selected = $(id);
+					scrollByController( this, settings.direction, selected.width(), 'prev');
+					break;
+
+				case 'vertical' :
+					var current = $(this).parent().parent().find('.current');
+					var id = current.find('button').attr('value');
+					var selected = $(id);
+					scrollByController( this, settings.direction, selected.height(), 'prev');
+					break;
+			}
+		});
 		
-		if(settings.direction == 'horizontal') {
-			paginations.on('click', 'li button', function() {
-		  		
-		  		var id = $(this).attr('value');
-		  		var selected = $(id);
-		  		var before = selected.parent().children('.current');
-		  		
-		  		var group_name = selected.attr('class').match(/pagination-[0-9][0-9]*/).toString();
-		  		var group_items = $('.' + group_name);
-		  		
-		  		$(this).parent().parent().find('.current').removeClass('current');
-		  		$(this).parent().addClass('current');
-		  		
-		  		before.removeClass('current');
-			  	selected.addClass('current');
-			  	
-			  	var offset = selected.attr('class').match(/no-[0-9]*/).toString().split('-').slice(-1);
-			  	var scroll_width = group_items.width() * (offset - 1);
+		pagination_element.on('click', 'li.next button', function() {
+			switch (settings.direction) {
+				case 'horizontal' :
+					var current = $(this).parent().parent().find('.current');
+					var id = current.find('button').attr('value');
+					var selected = $(id);
+					scrollByController( this, settings.direction, selected.width(), 'next');
+					break;
 
-			  	group_items.animate({ left: -scroll_width }, 1000);
-			  	
-		  	});	
-		}
-		else if(settings.direction == 'vertical') {
-			paginations.on('click', 'li button', function() {
-		  		
-		  		var id = $(this).attr('value');
-		  		var selected = $(id);
-		  		var before = selected.parent().children('.current');
-		  		
-		  		var group_name = selected.attr('class').match(/pagination-[0-9][0-9]*/).toString();
-		  		var group_items = $('.' + group_name);
-		  		
-		  		$(this).parent().parent().find('.current').removeClass('current');
-		  		$(this).parent().addClass('current');
-		  		
-		  		before.removeClass('current');
-			  	selected.addClass('current');
-			  	
-			  	var offset = selected.attr('class').match(/no-[0-9]*/).toString().split('-').slice(-1);
-			  	var scroll_height = group_items.height() * (offset - 1);
-			  	
-			  	group_items.animate({ top: -scroll_height }, 1000);
-			  	
-		  	});	
-		}
-
-	  	return this;
+				case 'vertical' :
+					var current = $(this).parent().parent().find('.current');
+					var id = current.find('button').attr('value');
+					var selected = $(id);
+					scrollByController( this, settings.direction, selected.height(), 'next');
+					break;
+			}
+		});
 	};
+	
+	$.fn.pagination.defaults = {
+		item: 1,
+		width: '200px',
+		height: '200px',
+		row: 3,
+		column: 3,
+		margin: '5px',
+		direction: 'vertical',
+		selector: 'li',
+		slideshow: false,
+		showtime: 5000,
+		controller: false,
+		touch: false,
+		find: false
+	}
+	
+	function scrollAnimation(object, direction, length, control) {
+	
+		var id = $(object).attr('value');
+  		var selected = $(id);
+  		var before = selected.parent().children('.current');
+  		
+  		var group_name = selected.attr('class').match(/pagination-[0-9][0-9]*/).toString();
+  		var group_items = $('.' + group_name);
+  		
+  		$(object).parent().parent().find('.current').removeClass('current');
+  		$(object).parent().addClass('current');
+  		
+  		before.removeClass('current');
+	  	selected.addClass('current');
+	  	
+	  	var offset = selected.attr('class').match(/no-[0-9]*/).toString().split('-').slice(-1);
+	  	var scroll_length = length * (offset - 1);
+	  	
+	  	if(control == 'next') {
+		  	scroll_length = length;
+	  	} else if( control == 'prev') {
+		  	scroll_length = -length;
+	  	}
+	  	
+		switch (direction) {
+			case 'horizontal': 
+				group_items.animate({ left: -scroll_length }, 1000);
+				break;
+			case 'vertical': 
+				group_items.animate({ top: -scroll_length }, 1000);
+				break;
+		}
+	}
+	
+	function scrollByController(object, direction, length, control) {
+	
+		var current = $(object).parent().parent().find('.current');
+		
+		var id = current.find('button').attr('value');
+		var selected = $(id);
+				
+		var group_name = selected.attr('class').match(/pagination-[0-9][0-9]*/).toString();
+  		var group_items = $('.' + group_name);
+		
+		
+		if(control == 'next') {
+		
+			var next = current.next();
+			if(next.hasClass('pager')) {
+			
+				next.addClass('current');
+				current.removeClass('current');
+				
+				var offset = next.find('button').attr('value').match(/no-[0-9]*/).toString().split('-').slice(-1);
+				var scroll_length = length * (offset - 1);
+				
+				switch (direction) {
+					case 'horizontal': 
+						group_items.animate({ left: -scroll_length }, 1000);
+						break;
+					case 'vertical': 
+						group_items.animate({ top: -scroll_length }, 1000);
+						break;
+				}
+			}
+		} else if(control == 'prev') {
+		
+			var prev = current.prev();
+			if(prev.hasClass('pager')) {
+			
+				prev.addClass('current');
+				current.removeClass('current');
+				
+				var offset = prev.find('button').attr('value').match(/no-[0-9]*/).toString().split('-').slice(-1);
+				var scroll_length = length * (offset - 1);
+				
+				switch (direction) {
+					case 'horizontal': 
+						group_items.animate({ left: -scroll_length }, 1000);
+						break;
+					case 'vertical': 
+						group_items.animate({ top: -scroll_length }, 1000);
+						break;
+				}
+			}
+		}
+	}
+	
 }( jQuery ));
